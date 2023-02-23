8ec67468ebd355075a8343ea38807ebaa9cd26d7
jayasekhar <do_not_send_mail@dummy_dnd.com>
2023-02-24 01:15:49 +0530

    Add palindrome number and string problems

--- problem-samples/src/main/java/com/jk/explore/problems/palindrome/number/PalindromeNumber.java
+++ problem-samples/src/main/java/com/jk/explore/problems/palindrome/number/PalindromeNumber.java
@@ -3,15 +3,39 @@ package com.jk.explore.problems.palindrome.number;
 public class PalindromeNumber {
 
     public boolean isPalindrome(int n) {
-        int originalNumber = n;
         if(n < 0) {
             return false;
         }
-        int reversedNumber = 0;
-        while(n > 0) {
-            reversedNumber = reversedNumber * 10 + (n % 10);
-            n = n / 10;
-        }
-        return reversedNumber == originalNumber;
+        int reversedNumber = reverse(n);
+        return reversedNumber == n;
     }
+
+    public int reverse(int number) {
+        int reversed = 0;
+        while (number > 0) {
+            int lastDigit = number % 10;
+            number = number / 10;
+            reversed = reversed * 10 + lastDigit;
+        }
+        return reversed;
+    }
+
+    public int reverseWithCharApi(int number) {
+        char[] chars = String.valueOf(number).toCharArray();
+        int left = 0;
+        int right = chars.length -1;
+        char temp;
+        while(left < right) {
+            temp = chars[right];
+            chars[right] = chars[left];
+            chars[left] = temp;
+            left++;
+            right--;
+        }
+        String reversedString = new String(chars);
+        return Integer.valueOf(reversedString).intValue();
+    }
+
+
+
 }
--- problem-samples/src/test/java/com/jk/explore/problems/palindrome/number/PalindromeNumberTest.java
+++ problem-samples/src/test/java/com/jk/explore/problems/palindrome/number/PalindromeNumberTest.java
@@ -1,8 +1,10 @@
 package com.jk.explore.problems.palindrome.number;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -20,12 +22,21 @@ public class PalindromeNumberTest {
         assertTrue(palindromeNumber.isPalindrome(121));
         assertTrue(palindromeNumber.isPalindrome(2332));
         assertFalse(palindromeNumber.isPalindrome(23456));
+        assertTrue(palindromeNumber.isPalindrome(1));
+        assertTrue(palindromeNumber.isPalindrome(0));
+        assertFalse(palindromeNumber.isPalindrome(23));
+        assertTrue(palindromeNumber.isPalindrome(22));
     }
 
     @Test
     public void isPalindromeForNegativeNumber() {
         assertFalse(palindromeNumber.isPalindrome(-121));
         assertFalse(palindromeNumber.isPalindrome(-12345));
+    }
+
+    @Test
+    public void reverseWithCharApi() {
+        assertEquals(palindromeNumber.reverseWithCharApi(123), 321);
     }
 
 }
